8c67514c2a475ceef484059afb5c272b6f8ccc37
Eric Dong <xdong17@my.bcit.ca>
2022-06-20 16:33:25 -0700

    Created a header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+import trollface from "../images/troll-face.png"
+
+export default function Header(){
+  return (
+    <header className="header">
+      <img src={trollface} alt="" className="header--image" />
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--project">React Project by Eric</h4>
+    </header>
+  )
+}
